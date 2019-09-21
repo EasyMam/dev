62da68d422dd912f6ca8465194c5853c6a542bd1
Sam <contactsamie@gmail.com>
2019-09-21 11:12:57 -0700

    update naming to camel case

--- index.js
+++ index.js
@@ -1,33 +1,33 @@
 (function(win) {
-  win.EasyMam = function(serverCall, loopCond, con, force) {
+  win.easymam = function(serverCall, loopCond, con, force) {
     return new Promise(function(resolve, reject) {
       if (!serverCall) {
         reject();
         return;
       }
       if (!force) {
-        if (win.EasyMam.inFlight) {
+        if (win.easymam.inFlight) {
           con && con.log && con.log("RETURNING STORED RESULT : ...");
-          if (win.EasyMam.result) {
-            resolve(win.EasyMam.result);
+          if (win.easymam.result) {
+            resolve(win.easymam.result);
             return;
           } else {
           }
         }
       }
 
-      win.EasyMam.inFlight = true;
+      win.easymam.inFlight = true;
       try {
         serverCall(x => {
-          win.EasyMam.result = x;
-          resolve(win.EasyMam.result);
+          win.easymam.result = x;
+          resolve(win.easymam.result);
           if (loopCond && loopCond(x)) {
             con &&
               con.log &&
               con.log("LOOP CONDITION MET - CALLING SERVER IN LOOP STARTS ...");
-            win.EasyMam(serverCall, loopCond, con, true);
+            win.easymam(serverCall, loopCond, con, true);
           } else {
-            win.EasyMam.inFlight = false;
+            win.easymam.inFlight = false;
           }
         });
       } catch (error) {
