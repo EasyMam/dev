79c0c7983548ab3bc1f759b95e229781348ea058
Sam <contactsamie@gmail.com>
2019-09-21 14:03:54 -0700

    add missing arg

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 /**
  * Adds two numbers together
- * 
+ *
  * @param {number} first The First Number
  * @param {number} second The Second Number
  * @returns {number}
@@ -39,50 +39,52 @@
       if (!force && win.easymam.inFlight && win.easymam.result) {
       } else {
         win.easymam.inFlight = true;
-       // setTimeout(() => {
-          try {
-            con && 
-              con.log &&
-              con.log("EASYMAM - LOOP CONDITION MET - CALLING SERVER NOW ...");
-            serverCall(x => {
-              win.easymam.result = x;
-              var canCallAgain = loopCond && loopCond(x);
-              if (canCallAgain) {
-                con &&
-                  con.log &&
-                  con.log(
-                    "EASYMAM - LOOP CONDITION MET - CALLING SERVER IN LOOP STARTS ..."
-                  );
-                win.easymam(
-                  serverCall,
-                  loopCond,
-                  callBackSuccess,
-                  callBackErrorOnLast,
-                  callBackSuccessOnExit,
-                  con,
-                  true
+        // setTimeout(() => {
+        try {
+          con &&
+            con.log &&
+            con.log("EASYMAM - LOOP CONDITION MET - CALLING SERVER NOW ...");
+          serverCall(x => {
+            win.easymam.result = x;
+            var canCallAgain = loopCond && loopCond(x);
+            if (canCallAgain) {
+              con &&
+                con.log &&
+                con.log(
+                  "EASYMAM - LOOP CONDITION MET - CALLING SERVER IN LOOP STARTS ..."
                 );
-              } else {
-                win.easymam.inFlight = false;
-                  if (force) {
-                    callBackSuccessOnExit(win.easymam.result);
-                  }
-              }
-              callBackSuccess(win.easymam.result);
-            
-              resolve(win.easymam.result);
-              return;
-            });
-          } catch (error) {
-            con && con.log && con.log("EASYMAM - SERVER ERROR...");
-            win.easymam.inFlight = false;
+              win.easymam(
+                serverCall,
+                loopCond,
+                callBackSuccess,
+                callBackErrorOnLast,
+                callBackSuccessOnExit,
+                minWaitMillisecondsBetweenCalls,
+                con,
 
-            try {
-              callBackErrorOnLast(error);
-            } catch (err) {}
-            reject(error);
+                true
+              );
+            } else {
+              win.easymam.inFlight = false;
+              if (force) {
+                callBackSuccessOnExit(win.easymam.result);
+              }
+            }
+            callBackSuccess(win.easymam.result);
+
+            resolve(win.easymam.result);
             return;
-          }
+          });
+        } catch (error) {
+          con && con.log && con.log("EASYMAM - SERVER ERROR...");
+          win.easymam.inFlight = false;
+
+          try {
+            callBackErrorOnLast(error);
+          } catch (err) {}
+          reject(error);
+          return;
+        }
         //}, minWaitMillisecondsBetweenCalls || 0);
       }
     });
