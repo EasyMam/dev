41c51cbda1a9c3ed39d4afb115411b9acf03192a
Sam <contactsamie@gmail.com>
2019-09-21 20:56:35 -0700

    include tests, update service call to use async

--- index.js
+++ index.js
@@ -1,10 +1,12 @@
-
 /*
   EasyMam - Your Easy strategic Memoization & Throttling
  
   A simple Memoization & Throttling Library for use in Web UI & NodeJS environment
   It allows only one call to be made to your service at any given time
   and within your set min time
+
+  serviceMethod is a promise or an async method that returns the result of your service when called
+  window.easymam(contextName).execute(serviceMethod, options);
 */
 var easymam = (function() {
   if (typeof easymam === "object") {
@@ -25,7 +27,19 @@ var easymam = (function() {
   var app = function(context) {
     context = context || "main";
     return {
-      execute: function(serverCall, options, force, callCount) {
+      promisify: function(f) {
+        return function(){
+            return new Promise(function(resolve, reject) {
+              f(resolve, reject);
+            });
+        }
+      },
+      wait:async function(t) {
+        return await this.promisify(function(resolve) {
+          setTimeout(resolve, t);
+        })();
+      },
+      execute: function(serviceMethod, options, force, callCount) {
         hanger.cache = hanger.cache || {};
         hanger.cache[context] = hanger.cache[context] || {};
         hanger.cache[context].lastCallAt =
@@ -34,116 +48,119 @@ var easymam = (function() {
         options = mergeOptions(options);
         callCount = callCount || 0;
         options.console.log("EASYMAM - NEW CALL RESULT : ...");
-        return new Promise(function(resolve, reject) {
-          if (!serverCall) {
+        return new Promise(async (resolve, reject) => {
+          if (!serviceMethod) {
             try {
               options.callBackErrorOnLast();
             } catch (err) {}
             reject();
             return;
-          }
+          }else{
+                var seconds = new Date() - hanger.cache[context].lastCallAt;
+                var pastLastCallPeriod =
+                  !hanger.cache[context].hasRunAtLeastOnce ||
+                  seconds > options.minWaitBetweenCalls;
 
-          var seconds = new Date() - hanger.cache[context].lastCallAt;
-          var pastLastCallPeriod =
-            !hanger.cache[context].hasRunAtLeastOnce ||
-            seconds > options.minWaitBetweenCalls;
+                var sinature =
+                  "CONTEXT : " +
+                  context +
+                  " - FORCE :" +
+                  force +
+                  " - CALL COUNT :" +
+                  callCount +
+                  " - LAST RUN AGO(ms): " +
+                  seconds +
+                  " - PASSED WAIT TIME : " +
+                  pastLastCallPeriod;
 
-          var sinature =
-            "CONTEXT : " +
-            context +
-            " - FORCE :" +
-            force +
-            " - CALL COUNT :" +
-            callCount +
-            " - LAST RUN AGO(ms): " +
-            seconds +
-            " - PASSED WAIT TIME : " +
-            pastLastCallPeriod;
-
-          if (pastLastCallPeriod) {
-            options.console.log("EASYMAM - WAIT TIME OVER ! ..." + sinature);
-            hanger.cache[context].lastCallAt = new Date();
-          } else {
-            options.console.log(
-              "EASYMAM -STILL WAITING FOR THE WAIT TIME TO BE OVER. SO FAR..." +
-                sinature
-            );
-          }
-          hanger.cache[context].hasRunAtLeastOnce = true;
-        //   if (
-        //     (!force && hanger.cache[context].inFlight) ||
-        //     !pastLastCallPeriod
-        //   ) {
-            
-        //   }
-          if (
-            (!force &&
-              hanger.cache[context].inFlight) ||
-            !pastLastCallPeriod
-          ) {
-
-            if (hanger.cache[context].result) {
-              options.console.log("EASYMAM - RETURNING RESULT ..." + sinature);
-              options.callBackSuccess(hanger.cache[context].result);
-              hanger.cache[context].isPending = true;
-              resolve(hanger.cache[context].result);
-              return;
-            } else {
-              options.console.log("EASYMAM - NO STORED ..." + sinature);
-              resolve(options.defaultResult);
-              return;
-            }
-          } else {
-            hanger.cache[context].inFlight = true;
-            hanger.cache[context].isPending = false;
-            try {
-              options.console.log("EASYMAM - CALLING FUN ..." + sinature);
-              serverCall(x => {
-                hanger.cache[context].result = x;
-                if (hanger.cache[context].isPending){
-                     options.console.log(
-                       "EASYMAM - THERE WAS A PENDING CALL SO I WILL MAKE ONE LAST CALL ..." + sinature
-                     );
-                }
-                  var canCallAgain =
-                    hanger.cache[context].isPending || options.loopCond(x);
-                if (canCallAgain) {
+                if (pastLastCallPeriod) {
                   options.console.log(
-                    "EASYMAM - LOOP CONDITION MET " + callCount
+                    "EASYMAM - WAIT TIME OVER ! ..." + sinature
                   );
-                  try {
-                    app[context].execute(
-                      serverCall,
-                      options,
-                      callCount + 1,
-                      true
+                  hanger.cache[context].lastCallAt = new Date();
+                } else {
+                  options.console.log(
+                    "EASYMAM -STILL WAITING FOR THE WAIT TIME TO BE OVER. SO FAR..." +
+                      sinature
+                  );
+                }
+                hanger.cache[context].hasRunAtLeastOnce = true;
+
+                if (
+                  (!force && hanger.cache[context].inFlight) ||
+                  !pastLastCallPeriod
+                ) {
+                  if (hanger.cache[context].result) {
+                    options.console.log(
+                      "EASYMAM - RETURNING RESULT ..." + sinature
                     );
-                  } catch (error) {
-                    //TODO : SOMETIME app BECONEs not defined. not sure why yet. catching it for now
-                    options.callBackSuccessOnExit(hanger.cache[context].result);                    
-                    hanger.cache[context].inFlight = false;
+                    options.callBackSuccess(hanger.cache[context].result);
+                    hanger.cache[context].isPending = true;
+                    resolve(hanger.cache[context].result);
+                    return;
+                  } else {
+                    options.console.log("EASYMAM - NO STORED ..." + sinature);
+                    resolve(options.defaultResult);
+                    return;
                   }
                 } else {
-                  hanger.cache[context].inFlight = false;
-                  if (force) {
-                    options.callBackSuccessOnExit(hanger.cache[context].result);
+                  hanger.cache[context].inFlight = true;
+                  hanger.cache[context].isPending = false;
+                  try {
+                    options.console.log("EASYMAM - CALLING FUN ..." + sinature);
+                    var x = await serviceMethod();
+                    hanger.cache[context].result = x;
+                    if (hanger.cache[context].isPending) {
+                      options.console.log(
+                        "EASYMAM - THERE WAS A PENDING CALL SO I WILL MAKE ONE LAST CALL ..." +
+                          sinature
+                      );
+                    }
+                    var canCallAgain =
+                      hanger.cache[context].isPending || options.loopCond(x);
+                    if (canCallAgain) {
+                      options.console.log(
+                        "EASYMAM - LOOP CONDITION MET " + callCount
+                      );
+                      try {
+                        app[context].execute(
+                          serviceMethod,
+                          options,
+                          callCount + 1,
+                          true
+                        );
+                      } catch (error) {
+                        //TODO : SOMETIME app BECONEs not defined. not sure why yet. catching it for now
+                        options.callBackSuccessOnExit(
+                          hanger.cache[context].result
+                        );
+                        hanger.cache[context].inFlight = false;
+                      }
+                    } else {
+                      hanger.cache[context].inFlight = false;
+                      if (force) {
+                        options.callBackSuccessOnExit(
+                          hanger.cache[context].result
+                        );
+                      }
+                    }
+                    options.callBackSuccess(hanger.cache[context].result);
+                    resolve(hanger.cache[context].result);
+                    return;
+                  } catch (error) {
+                    options.console.log("EASYMAM - SERVER ERROR..." + sinature);
+                    hanger.cache[context].inFlight = false;
+                    try {
+                      options.callBackErrorOnLast(error);
+                    } catch (err) {}
+                    reject(error);
+                    return;
                   }
+                  //}, options.minWaitBetweenCalls || 0);
                 }
-                options.callBackSuccess(hanger.cache[context].result);
-                resolve(hanger.cache[context].result);
-                return;
-              });
-            } catch (error) {
-              options.console.log("EASYMAM - SERVER ERROR..." + sinature);
-              hanger.cache[context].inFlight = false;
-              try {
-                options.callBackErrorOnLast(error);
-              } catch (err) {}
-              reject(error);
-              return;
-            }
-            //}, options.minWaitBetweenCalls || 0);
           }
+
+        
         });
       }
     };
--- tests.js
+++ tests.js
@@ -1,3 +1,151 @@
-QUnit.test("hello test", function(assert) {
-  assert.ok(1 == "1", "Passed!");
+QUnit.test("Single initial call", async assert => {
+  var sequenceOfEvent = "";
+  var options = {
+    loopCond: function(x) {
+      sequenceOfEvent += "4";
+      return x.processing;
+    },
+    callBackSuccess: function() {
+      sequenceOfEvent+="5";
+    },
+    callBackErrorOnLast: function() {
+      sequenceOfEvent += "7";
+    },
+    callBackSuccessOnExit: function(x) {
+      sequenceOfEvent += "6";
+    },
+    minWaitBetweenCalls: 1000
+  };
+  sequenceOfEvent += "1";
+  var x = await window.easymam("context").execute(async () => {
+    sequenceOfEvent += "2";
+    await window.easymam().wait(1000);
+    sequenceOfEvent += "3";
+    return {
+      processing: false,
+      data: new Date().getTime()
+    };
+  }, options);
+
+  assert.ok(sequenceOfEvent == "12345", "Correct sequence of event was executed");
+});
+
+
+QUnit.test(
+  "Double initial call",
+  async assert => {
+    var sequenceOfEvent = "";
+    var options = {
+      loopCond: function(x) {
+        sequenceOfEvent += "4";
+        return x.processing;
+      },
+      callBackSuccess: function() {
+        sequenceOfEvent += "5";
+      },
+      callBackErrorOnLast: function() {
+        sequenceOfEvent += "7";
+      },
+      callBackSuccessOnExit: function(x) {
+        sequenceOfEvent += "6";
+      },
+      minWaitBetweenCalls: 1000
+    };
+    sequenceOfEvent += "1";
+
+    for (let index = 0; index < 2; index++) {
+      var x = await window.easymam("context").execute(async () => {
+        sequenceOfEvent += "2";
+        await window.easymam().wait(1000);
+        sequenceOfEvent += "3";
+        return {
+          processing: false,
+          data: new Date().getTime()
+        };
+      }, options);
+    }
+
+    assert.ok(
+      sequenceOfEvent == "123452345",
+      "Correct sequence of event was executed"
+    );
+  }
+);
+
+
+QUnit.test(" Tripple initial call timely spaced out", async assert => {
+  var sequenceOfEvent = "";
+  var options = {
+    loopCond: function(x) {
+      sequenceOfEvent += "4";
+      return x.processing;
+    },
+    callBackSuccess: function() {
+      sequenceOfEvent += "5";
+    },
+    callBackErrorOnLast: function() {
+      sequenceOfEvent += "7";
+    },
+    callBackSuccessOnExit: function(x) {
+      sequenceOfEvent += "6";
+    },
+    minWaitBetweenCalls: 1000
+  };
+  sequenceOfEvent += "1";
+
+  for (let index = 0; index < 3; index++) {
+    await window.easymam().wait(1000);
+    var x = await window.easymam("context").execute(async () => {
+      sequenceOfEvent += "2";
+      await window.easymam().wait(1000);
+      sequenceOfEvent += "3";
+      return {
+        processing: false,
+        data: new Date().getTime()
+      };
+    }, options);
+  }
+
+  assert.ok(
+    sequenceOfEvent == "1234523452345",
+    "Correct sequence of event was executed"
+  );
+});
+
+
+QUnit.test("Single initial call - service throws", async assert => {
+  var sequenceOfEvent = "";
+  var options = {
+    loopCond: function(x) {
+      sequenceOfEvent += "4";
+      return x.processing;
+    },
+    callBackSuccess: function() {
+      sequenceOfEvent += "5";
+    },
+    callBackErrorOnLast: function() {
+      sequenceOfEvent += "7";
+    },
+    callBackSuccessOnExit: function(x) {
+      sequenceOfEvent += "6";
+    },
+    minWaitBetweenCalls: 1000
+  };
+  sequenceOfEvent += "1";
+try {
+    var x = await window.easymam("context").execute(async () => {
+      sequenceOfEvent += "2";
+      await window.easymam().wait(1000);
+      sequenceOfEvent += "3";
+
+      throw "server error";
+    }, options);
+} catch (error) {
+  sequenceOfEvent += "8";
+}
+
+  assert.ok(
+    sequenceOfEvent == "12378",
+    "Correct sequence of event was executed"
+  );
 });
