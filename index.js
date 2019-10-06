575086e024c73e321f8eac8c188cad4a650d3bce
Sam <contactsamie@gmail.com>
2019-10-06 01:20:18 -0700

    include monitor

--- index.js
+++ index.js
@@ -212,9 +212,41 @@ var easymam = (function() {
       }
     };
   };
-  
+  var monitor = (function() {
+  return {
+    run: async function(opt) {
+      var options = {
+        loopCond: function(x) {
+          return true;
+        },
+        callBackErrorOnLast: function() {},
+        callBackSuccess: function(data) {
+          opt.onSuccess && opt.onSuccess(data);
+        },
+        minWaitBetweenCalls: opt.interval
+      };
+
+      var handle = easymam();
+      //chrome.exe --user-data-dir="C:/Chrome dev session" --disable-web-security
+      await handle.execute(
+        handle.promisify(resolve => {
+          $.get(opt.url, function(data) {
+            resolve(data);
+          });
+        }),
+        options
+      );
+    }
+  };
+})();
+  app.monitor = monitor;
   return app;
 })();
+
+
+
+
+
 var $easymam = function(f,opt,context){
  return easymam(context).execute(f,opt);
 };
